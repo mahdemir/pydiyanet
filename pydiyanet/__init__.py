d211e29ee73c070a47189ac4e6d42a0ebd42782f
mahdemir <mahmutemindemir@gmail.com>
2026-02-28 20:52:05 +0000

    chore: testing gh wf

--- pydiyanet/__init__.py
+++ pydiyanet/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from .client import DiyanetApiClient
 from .exceptions import DiyanetApiError, DiyanetAuthError, DiyanetConnectionError
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = [
     "DiyanetApiClient",
